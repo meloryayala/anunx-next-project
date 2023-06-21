d2c5ea39344e9ebc2f8e9eb3f520bec4e90cbf6b
Melory Ayala <melory.ayala@gmail.com>
2023-06-21 17:21:19 +0200

    Initial structure + material.ui

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@material-ui/core/styles'
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#000000",
+        },
+        secondary: {
+            main: "#555555",
+        },
+    }
+})
+
+export default theme
